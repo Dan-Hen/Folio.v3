0b8014e74b1db55c7a117981bfac511626f430d5
Dan-Hen <leo.clot16@gmail.com>
2021-12-10 00:42:50 +0100

    add three.js torus

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-console.log('Hello World')
+import '../src/style.scss';
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+//set the size of the camera. In order : TODO
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+    //set the background of the canvas transparent
+    alpha: true,
+    canvas: document.querySelector('#bg'),
+  });
+
+//set the size of canvas by the size of the screen
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+//add mesh / geometry and materials
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const material = new THREE.MeshBasicMaterial ( {color: 0XEC7442, wireframe: true } );
+const torus = new THREE.Mesh ( geometry, material);
+
+//render the scene
+scene.add (torus) 
+
+//render the scene
+function animate() {
+    requestAnimationFrame ( animate );
+    renderer.render (scene, camera);
+}
+
+animate()
