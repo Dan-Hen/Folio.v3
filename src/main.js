888aaa8b3a2662e5b64b963a4011f08235b3ff37
Dan-Hen <leo.clot16@gmail.com>
2022-01-15 01:57:05 +0100

    fix barba

--- src/main.js
+++ src/main.js
@@ -13,13 +13,13 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 
 //set the size of the camera. In order : field of view, aspect ratio, view frustum
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-    //set the background of the canvas transparent
-    alpha: true,
-    canvas: document.querySelector('#bg'),
-  });
+  //set the background of the canvas transparent
+  alpha: true,
+  canvas: document.querySelector('#bg'),
+});
 
 //set the size of canvas by the size of the screen
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -27,103 +27,170 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 //add mesh / geometry and materials
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshBasicMaterial ( {color: 0XEC7442, wireframe: true } );
-const torus = new THREE.Mesh ( geometry, material);
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({ color: 0XEC7442, wireframe: true });
+const torus = new THREE.Mesh(geometry, material);
 
 //render the scene
-scene.add (torus) 
+scene.add(torus)
 
 //render the scene
 function animate() {
-    requestAnimationFrame ( animate );
-    renderer.render (scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
 animate()
 
-// project text length
+function HomeEnter() {
 
-const getFontSize = (textLength) => {
-  const FirstSyl = document.getElementsByClassName("hero-title")[0];
-  const WidthFiSyl = getComputedStyle(FirstSyl)
-  const SecondSyl = document.getElementsByClassName("hero-title")[1];
-  const WidthSecSyl = getComputedStyle(SecondSyl)
-  const length = parseInt(WidthFiSyl.width, 10) + parseInt(WidthSecSyl.width,);
+  // sccs switch
+  var clicked = false;
+  document.getElementById("light").onclick = function () { changeTheme() };
+  function changeTheme() {
+    if (clicked) {
 
-  const baseSize = 28;
-  let fontSize = length 
-  if (length <= 1809) {
-    fontSize = baseSize;
-  } else {
-    fontSize = baseSize - ((length - 1809) * 0.0138);
+      document.querySelector(':root').style.setProperty('--main-bg-color', '#DEDEDE');
+      document.querySelector(':root').style.setProperty('--primary-color', '#101010');
+      document.querySelector(':root').style.setProperty('--primary-faded', '#B2B2B2');
+      clicked = false;
+    }
+    else {
+      document.querySelector(':root').style.setProperty('--main-bg-color', '#000000');
+      document.querySelector(':root').style.setProperty('--primary-color', '#DEDEDE');
+      document.querySelector(':root').style.setProperty('--primary-faded', '#2C2C2C');
+      clicked = true;
+    }
   }
-  return `${fontSize}vw`
 }
 
-const boxes = document.querySelectorAll('.hero h1')
-  
-boxes.forEach(box => {
-  box.style.fontSize = getFontSize(box.textContent.length)
+function HomeScroll() {
+
+}
+
+function ProjectLaunch() {
+
+  // project text length
+  const getFontSize = (textLength) => {
+    const FirstSyl = document.getElementsByClassName("hero-title")[0];
+    const WidthFiSyl = getComputedStyle(FirstSyl);
+    const SecondSyl = document.getElementsByClassName("hero-title")[1];
+    const WidthSecSyl = getComputedStyle(SecondSyl);
+    const length = parseInt(WidthFiSyl.width, 10) + parseInt(WidthSecSyl.width,);
+
+    const baseSize = 28;
+    let fontSize = length
+    if (length <= 1809) {
+      fontSize = baseSize;
+    } else {
+      fontSize = baseSize - ((length - 1809) * 0.0138);
+    }
+    return `${fontSize}vw`
+  }
+
+  const boxes = document.querySelectorAll('.hero h1')
+
+  boxes.forEach(box => {
+    box.style.fontSize = getFontSize(box.textContent.length)
+  })
+
+  //slider
+  function slider() {
+    var slides = document.getElementById('slides'),
+      slide = document.getElementsByClassName('slide'),
+      slidesLength = slide.length,
+      firstSlide = slide[0],
+      lastSlide = slide[slidesLength - 1],
+      cloneFirst = firstSlide.cloneNode(true),
+      cloneLast = lastSlide.cloneNode(true),
+      slideMargin = getComputedStyle(firstSlide),
+      slideWidth = firstSlide.getBoundingClientRect().width + parseFloat(slideMargin.getPropertyValue('margin-left')) + parseFloat(slideMargin.getPropertyValue('margin-right')),
+      slidesSize = slideWidth * slidesLength,
+      x = document.getElementById('slides').scrollLeft;
+
+    slides.scrollTo(304, 0);
+
+    console.log(slidesSize)
+
+    slides.appendChild(cloneFirst);
+    slides.insertBefore(cloneLast, firstSlide);
+
+    slides.scrollTo(304, 0);
+  }
+  slider()
+
+  document.getElementById("slides").addEventListener("scroll", infiniteSlider);
+
+  function infiniteSlider() {
+    var slides = document.getElementById('slides'),
+
+      x = document.getElementById('slides').scrollLeft;
+
+    if (x < 1) {
+      slides.scrollTo(2099, 0);
+    }
+
+    else if (x > 2099) {
+      slides.scrollTo(1, 0);
+    }
+  }
+}
+
+function ProjectScroll() {
+}
+
+function ProjectLeave() {
+}
+
+barba.init({
+  debug: true,
+  transitions: [
+    {
+      name: 'opacity-transition',
+      leave(data) {
+
+      },
+      enter(data) {
+
+      }
+    }
+  ],
+  views: [
+    {
+      namespace: 'project',
+      beforeEnter({ next }) {
+        ProjectLaunch()
+      },
+      afterEnter({ next }) {
+
+      },
+      beforeLeave({ next }) {
+
+      }
+    },
+    {
+      namespace: 'menu',
+      beforeEnter({ next }) {
+        
+      },
+      afterEnter({ next }) {
+
+      },
+      beforeLeave({ next }) {
+
+      }
+    },
+    {
+      namespace: 'home',
+      beforeEnter({ next }) {
+        HomeEnter()
+      },
+      afterEnter({ next }) {
+
+      },
+      beforeLeave({ next }) {
+
+      }
+    }
+  ]
 })
-
-//slider
-function slider(){
-  var slides = document.getElementById('slides'),
-  slide = document.getElementsByClassName('slide'),
-  slidesLength = slide.length,
-  firstSlide = slide[0],
-  lastSlide = slide[slidesLength - 1],
-  cloneFirst = firstSlide.cloneNode(true),
-  cloneLast = lastSlide.cloneNode(true),
-  slideMargin = getComputedStyle(firstSlide),
-  slideWidth = firstSlide.getBoundingClientRect().width + parseFloat(slideMargin.getPropertyValue('margin-left')) + parseFloat(slideMargin.getPropertyValue('margin-right')),
-  slidesSize = slideWidth * slidesLength,
-  x = document.getElementById('slides').scrollLeft;
-  
-  slides.scrollTo(304, 0);
-
-  console.log(slidesSize)
-
-  slides.appendChild(cloneFirst);
-  slides.insertBefore(cloneLast, firstSlide);
-  
-  slides.scrollTo(304, 0);
-}
-slider()
-
-document.getElementById("slides").addEventListener("scroll", infiniteSlider);
-
-function infiniteSlider() {
-  var slides = document.getElementById('slides'),
-  
-  x = document.getElementById('slides').scrollLeft;
-
-  if (x < 1){
-    slides.scrollTo(2099, 0);
-  }
-
-  else if (x > 2099) {
-    slides.scrollTo(1, 0);
-  }
-}
-
-
-// sccs switch
-var clicked = false;
-document.getElementById("light").onclick=function(){changeTheme()};
-function changeTheme(){
-if (clicked) {
-  
-    document.querySelector(':root').style.setProperty('--main-bg-color', '#DEDEDE');
-    document.querySelector(':root').style.setProperty('--primary-color', '#101010');
-    document.querySelector(':root').style.setProperty('--primary-faded', '#B2B2B2');
-    clicked = false;
-  }
-else {
-    document.querySelector(':root').style.setProperty('--main-bg-color', '#000000');
-    document.querySelector(':root').style.setProperty('--primary-color', '#DEDEDE');
-    document.querySelector(':root').style.setProperty('--primary-faded', '#2C2C2C');
-    clicked = true;
-  }
-}
